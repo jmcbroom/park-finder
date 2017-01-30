0a6a3605edf843fad9b081ed8a0ce21811900fa8
Jimmy McBroom <james.mcbroom@gmail.com>
2017-01-30 14:18:13 -0500

    add a whole bunch of stuff

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var mapboxgl = require('mapbox-gl');
 var _ = require('lodash');
-var bbox = require('@turf/bbox')
+var bbox = require('@turf/bbox');
+var centroid = require('@turf/centroid');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2l0eW9mZGV0cm9pdCIsImEiOiJjaXZvOWhnM3QwMTQzMnRtdWhyYnk5dTFyIn0.FZMFi0-hvA60KYnI-KivWg';
 
-// here are the filters
+// lookup object for the filters.
+// these should match columns in the table that are set to 1 or null.
 var FILTERS = {
   "aerobics": "Aerobics",
   "archery": "Archery",
@@ -34,11 +36,10 @@ var FILTERS = {
   "zumba": "Zumba"
 }
 
-// make a map objects
+// make a map object
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/cityofdetroit/cix0w7n08000v2qpb2q15raq1',
-    doubleClickZoom: false,
     zoom: 10.7,
     center: [-83.091, 42.350],
     // :triangular_ruler:
@@ -50,20 +51,29 @@ var map = new mapboxgl.Map({
     ]
 });
 
+// add nav and geolocation controls to the map
 var nav = new mapboxgl.NavigationControl();
-map.addControl(nav, 'bottom-left');
+var geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    }
+});
+map.addControl(nav, 'top-left');
+map.addControl(geolocate, 'top-left');
 
 var popup = new mapboxgl.Popup({
     closeButton: false
 });
 
-
+// do all the things when the map loads
 map.on('load', function() {
+  // add the GeoJSON from our ArcServer
   map.addSource('parks', {
     type: 'geojson',
     data: 'http://gis.detroitmi.gov/arcgis/rest/services/Parks/ParksAndRec/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=5&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=geojson'
   })
 
+  // add a fill layer
   map.addLayer({
     "id": "parks-fill",
     "type": "fill",
@@ -76,6 +86,8 @@ map.on('load', function() {
         "fill-opacity": 0.25
     }
   })
+
+  // add an outline layer
   map.addLayer({
     "id": "parks-line",
     "type": "line",
@@ -99,13 +111,39 @@ map.on('load', function() {
     }
   })
 
-  // attach ze filters
+  // vars for containers
   var filterPicker = document.getElementById('filter-picker');
   var filterPickerContainer = document.getElementById('filter-picker-container');
   var parkList = document.getElementById('list-container');
-  var filter = ['any'];
+  var parkDetails = document.getElementById('detail-container');
+
+  // this button resets everything
+  var resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click', function() {resetMap();});
+
+  // this button zooms out to Detroit
+  var zoomButton = document.getElementById('zoomToCity');
+  zoomButton.addEventListener('click', function() {zoomToCity();});
+
+  // start with no filter, and no picked filters
+  var filter = null;
   var pickedFilters = [];
 
+  // i found this on stack overflow. extends the DOM to add a .remove() function for a NodeList
+  // https://stackoverflow.com/questions/3387427/remove-element-by-id
+  Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+  }
+  NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+      for(var i = this.length - 1; i >= 0; i--) {
+          if(this[i] && this[i].parentElement) {
+              this[i].parentElement.removeChild(this[i]);
+          }
+      }
+  }
+
+  // this is from the Mapbox example for "list all rendered features"
+  // https://www.mapbox.com/mapbox-gl-js/example/filter-features-within-map-view/
   function getUniqueFeatures(array, comparatorProperty) {
       var existingFeatureKeys = {};
       // Because features come from tiled vector data, feature geometries may be split
@@ -123,84 +161,175 @@ map.on('load', function() {
       return uniqueFeatures;
   }
 
-  function listUpdateHandler() {
-    var parks = map.queryRenderedFeatures({
-        layers: ['parks-fill'],
-        filter: filter
+  // when you click on a park, fly to it and populate the details-container
+  function clickOnPark(p) {
+    // zoom to the park, but not too close
+    var flyBbox = bbox(p.geometry);
+      map.fitBounds([
+        [flyBbox[0], flyBbox[1]],
+        [flyBbox[2], flyBbox[3]]
+      ], { padding: 200, maxZoom: 15.5 });
+
+      // get all the amenities for the park
+      var parkAmenities = [];
+      Object.keys(p.properties).forEach(function(a){
+        if (p.properties[a] == 1) {
+          parkAmenities.push(FILTERS[a]);
+        }
       });
+
+      // html for all parks
+      var parkHtml = `
+      <b><span>${p.properties.name}</b></span>
+      <hr>
+      <span><b>Available activities:<br/></b> ${parkAmenities.join(', ')}</span>
+      `
+
+      // if it has a rec center, toss this in there
+      if (p.properties.rec_center_name != 'null') {
+        var recCtrHtml = `
+          <span><b>Recreation Center:</b><br/> ${p.properties.rec_center_name}</span>
+          <span><b>Hours of Operation:</b><br/> ${p.properties.opening_hours}</span>
+          <br/>
+        `
+        parkDetails.innerHTML = recCtrHtml + parkHtml;
+      }
+      else {
+        parkDetails.innerHTML = parkHtml;
+      }
+  }
+
+
+  function resetMap() {
+    zoomToCity();
+
+    // remove filters
+    map.setFilter('parks-fill', null);
+    map.setFilter('parks-line', null);
+
+    // remove filter items
+    var filtersToRemove = document.getElementsByClassName("filter-item").remove();
+
+    // clear park details
+    parkDetails.innerHTML = '';
+    // clear park list
     while (parkList.firstChild) {
       parkList.removeChild(parkList.firstChild);
     }
-    console.log(getUniqueFeatures(parks, 'ogc_fid'))
-    var features = getUniqueFeatures(parks, 'ogc_fid')
-    if (features) {
-      features.forEach(function(p){
-        // console.log(p.geometry.coordinates[0][0])
-        var park = document.createElement('span');
-        park.innerHTML = `<b>${p.properties.name}</b><br /><i>(${p.properties.address})</i>`
-        park.addEventListener('mouseover', function() {
-            console.log(p);
-            console.log(bbox(p.geometry))
-            // Highlight corresponding feature on the map
-            popup.setLngLat(p.geometry.coordinates[0][0])
-                .setText(p.properties.name + ' (' + p.properties.address + ')')
-                .addTo(map);
-        });
-        park.addEventListener('click', function() {
-          var flyBbox = bbox(p.geometry)
-            map.fitBounds([
-              [flyBbox[0], flyBbox[1]],
-              [flyBbox[2], flyBbox[3]]
-            ]);
-        });
-        parkList.appendChild(park)
-      })
-    }
+    // empty pickedFilters
+    pickedFilters = [];
   }
 
+  function zoomToCity(){
+    map.fitBounds([
+      [-83.2878, 42.2551],
+      [-82.9104, 42.4502]
+    ], { padding: 50})
+  }
+
+  // this fires when the filters change.
   function filterUpdateHandler() {
-    filter = ['any']
-    pickedFilters.forEach(function(f){
-      filter.push(['==', f, 1])
-    });
+    // set no filter
+    filter = null;
+    // are some filters picked? add the filter
+    if (pickedFilters.length > 0){
+      filter = ['any'];
+      pickedFilters.forEach(function(f){
+        filter.push(['==', f, 1])
+      });
+    }
+    // set that filter
     map.setFilter('parks-fill', filter);
     map.setFilter('parks-line', filter);
+
+    // give a slight delay before populating the parks list
+    setTimeout(function(){
+      var parks = map.queryRenderedFeatures({
+          layers: ['parks-fill'],
+          filter: filter
+        });
+
+      // remove all listed parks
+      while (parkList.firstChild) {
+        parkList.removeChild(parkList.firstChild);
+      }
+      // get all the unique rendered parks
+      var features = getUniqueFeatures(parks, 'ogc_fid')
+      if (features) {
+        // push them to the list-container
+        features.forEach(function(p){
+          var park = document.createElement('span');
+          park.innerHTML = `<b>${p.properties.name}</b><br /><i>(${p.properties.address})</i>`
+          park.addEventListener('mouseover', function() {
+              // on mouseover, highlight the park on the map
+              var geomCentroid = centroid(p.geometry)
+              popup.setLngLat(centroid(p.geometry).geometry.coordinates)
+                  .setText(p.properties.name + ' (' + p.properties.address + ')')
+                  .addTo(map);
+          });
+          // if you click on the entry, we want to fly there and add the details
+          park.addEventListener('click', function() {
+            clickOnPark(p);
+          });
+          parkList.appendChild(park)
+        })
+      }
+    }, 100);
   }
 
+  // fires when a filter is added
   function addFilterHandler(e){
     var selected = document.createElement('span')
     selected.textContent = e.target.value + " ";
     selected.className = "filter-item";
+    selected.id = e.target.selectedOptions[0].id
+    // remove it if you click it
     selected.onclick = function(){
-      removeFilterHandler(e.target.selectedOptions[0].id)
+      removeFilterHandler(this.id)
       this.parentNode.removeChild(this);
     }
     filterPickerContainer.appendChild(selected);
+    // add the filter to pickedFilters
     pickedFilters.push(e.target.selectedOptions[0].id)
+    // call the update function
     filterUpdateHandler();
-    listUpdateHandler();
   };
 
+  // fires when a filter is removed
   function removeFilterHandler(id){
-    console.log(id)
     _.remove(pickedFilters, function(d){
       return d == id;
     });
-    console.log(pickedFilters)
+    // if we remove the last filter, reset the whole thing
+    if (pickedFilters.length == 0) {
+      resetMap();
+    };
+    // call the update function
     filterUpdateHandler();
-    listUpdateHandler();
-    // setTimeout(function(){ listUpdateHandler() }, 100);
   };
 
+  // call addFilterHandler if you pick a new filter from the dropdown
   filterPicker.addEventListener('change', function(e){ addFilterHandler(e)} );
 
-  map.on('moveend', function() {listUpdateHandler()});
+  // if you move the map; update the filtered maps
+  map.on('moveend', function() {filterUpdateHandler()});
 
+  // we want to add the park detail if you click on a park
+  map.on('click', function(e){
+    var features = map.queryRenderedFeatures(e.point, { layers: ['parks-fill'] });
+    if (!features.length) {
+        return;
+    }
+    else {
+      clickOnPark(features[0]);
+    }
+  })
+
+  // populate the filter list
   Object.keys(FILTERS).forEach(function(f){
     var option = document.createElement('option');
     option.innerHTML = FILTERS[f];
     option.id = f;
     filterPicker.appendChild(option);
   })
-
 })
